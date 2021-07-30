c8f60c6056df4213a20dfe0991b55410c808ea1f
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-07-30 20:24:50 +0900

    色変更
    Co-authored-by: WinterYukky <WinterYukky@users.noreply.github.com>

---
+++ assets/css.vue
@@ -0,0 +1,8 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Shippori+Mincho&display=swap');
+#app {
+  font-family: 'Shippori Mincho', serif;
+
+  background-color: #f7c3bf;
+}
+</style>
